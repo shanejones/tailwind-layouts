df959fe12cc52bf2b70ff7bc8f2368b85e81b2d0
Shane Jones <shane@under2.agency>
2019-11-23 08:40:49 +0000

    Tailwind CSS and not Tailwinds CSS!

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,14 +1,14 @@
 @extends('_layouts.master')
 
 @section('head-meta')
-    <title>TailwindsCSS Layouts and Templates</title>
+    <title>TailwindCSS Layouts and Templates</title>
     <meta name="description" content="A selection of layouts, blocks and full templates using Flat HTML, Tailwind CSS and VanillaJS. Built by Shane Jones.">
 @endsection
 
 @section('content')
 
 <main class="p-16">
-    <h1 class="text-3xl font-light text-black">TailwindsCSS Layouts and Templates</h1>
+    <h1 class="text-3xl font-light text-black">TailwindCSS Layouts and Templates</h1>
     <p class="mt-4 text-gray-600">A selection of layouts, blocks and full templates using Flat HTML, Tailwind CSS and VanillaJS. Built by Shane Jones.</p>
 </main>
 
